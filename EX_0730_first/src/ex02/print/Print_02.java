a621cd877010f68d3471680f9de6bb351d397d73
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2024-07-30 17:43:58 +0900

    Add files via upload

---
+++ EX_0730_first/src/ex02/print/Print_02.java
@@ -0,0 +1,13 @@
+package ex02.print;
+
+public class Print_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+		System.out.println("Java World");
+		
+		
+	}
+
+}
